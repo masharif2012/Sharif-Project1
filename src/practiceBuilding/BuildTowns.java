97b4a62dfc1b7735a351b7615a8c7277d5c63d36
masharif2012 <isharifpalash@gmail.com>
2018-11-25 19:13:08 -0500

    first commit

---
+++ src/practiceBuilding/BuildTowns.java
@@ -0,0 +1,22 @@
+package practiceBuilding;
+
+
+
+public class BuildTowns {
+	
+	int numberOfSt = 12;
+	int numbers = 200;
+	String name = "Woodside,NY";
+	
+	
+	public static void main (String[]args) {
+		
+		Town myTownShip = new Town();
+		
+		//System.out.println(numbers);
+		
+		
+	
+	}
+
+}
---
+++ src/practiceBuilding/Town.java
@@ -0,0 +1,13 @@
+package practiceBuilding;
+
+public class Town {
+	
+	int numberOfSt = 12;
+	int number = 200;
+	String name = "Woodside,NY";
+	
+	
+	
+	
+
+}
